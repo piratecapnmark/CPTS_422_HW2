b8da977a5bf8b132950da44d788e9864d71a4afc
SandMan36 <yangjiurong0@gmail.com>
2014-09-12 14:38:51 -0700

    File From Teacher

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+// Cpt S 422 HW2
+
+#include <iostream>
+#include "Stream.hpp"
+
+using namespace CS422;
+
+int main(int argc, const char* argv[])
+{
+	// In other files you will implement the classes that inherit from stream.
+	// Create instances of such objects as needed here (make sure you are creating 
+	// each type of stream that you implemented).
+	// Write Stream unit testing functions and kick off the tests from here in main.
+	
+	return 0;
+}
