a1df43edb49999f2d6ff3452a15c29caed6d973a
SandMan36 <yangjiurong0@gmail.com>
2014-09-12 18:56:53 -0700

    MemoryStream

---
+++ MemoryStream.hpp
@@ -0,0 +1,82 @@
+#include "Stream.hpp"
+
+using namespace std;
+using namespace CS422;
+
+class MemoryStream : public Stream {
+
+	public:
+		bool CanSeek()
+		{
+			return true;
+		}
+		bool CanRead()
+		{
+			return true;
+		}
+		bool CanWrite()
+		{
+			return true;
+		}
+		void Flush()
+		{
+			cout << "Something";
+		}
+		i64 GetLength() const
+		{
+			return streamSize;
+		}
+		i64 GetPosition() const
+		{
+			return current;
+		}
+		int Read(void* buf, int byteCount)
+		{
+			if (streamSize - current < byteCount)
+			{
+				cout << "The stream is not large enough for you to read " << byteCount << " bytes" << endl;
+				return 0;
+			}
+			else
+			{
+				char * temp = (char *)memStream;
+				memcpy(buf, (temp + current), byteCount);
+			}
+			return byteCount;
+		}
+		i64 SetPosition(i64 position)
+		{
+			if (position < streamSize)
+			{
+				current = (int)position;
+				return current;
+			}
+			else
+			{
+				cout << "The position you are trying to set is larger than the stream size" << endl;
+				return 0;
+			}
+		}
+		int Write(const void* buf, int byteCount)
+		{
+			int i = 0;
+		    void *pointer;
+			pointer = malloc(streamSize + byteCount);
+			memcpy(pointer, memStream, streamSize);
+			cout << (char*)pointer << endl;
+			memcpy(((char*)pointer + streamSize), buf, byteCount);
+			
+			for (i = 0; i < (streamSize + byteCount); i++)
+			{
+				printf("%c", *((char*)pointer + i));
+			}
+
+			streamSize += byteCount;
+			memStream = pointer;
+			return byteCount;
+		}
+	private:
+		int streamSize = 0;
+		int current = 0;
+	    void *memStream;
+};
--- Source.cpp
+++ Source.cpp
@@ -2,15 +2,25 @@
 
 #include <iostream>
 #include "Stream.hpp"
-
+#include "MemoryStream.hpp"
 using namespace CS422;
 
 int main(int argc, const char* argv[])
 {
+	MemoryStream i;
+	char * str = "what the fuck is this shit";
+	char * str1 = "Please work";
+	i.Write(str, 27);
+	i.Write(str1, 12);
+	char read[30];
+	i.SetPosition(5);
+	i.Read(read, 30);
+	cout << read << endl;
+	system("pause");
+	
 	// In other files you will implement the classes that inherit from stream.
 	// Create instances of such objects as needed here (make sure you are creating 
 	// each type of stream that you implemented).
 	// Write Stream unit testing functions and kick off the tests from here in main.
-	
 	return 0;
 }
